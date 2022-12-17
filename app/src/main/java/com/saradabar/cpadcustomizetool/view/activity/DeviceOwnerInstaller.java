80bb570d5d4c473d4f4e523033c90cb7e00faa21
Bamb85 <120371803+Bamb85@users.noreply.github.com>
2022-12-17 21:16:16 +0900

    アクティビティと文字列を追加

---
+++ app/src/main/java/com/saradabar/cpadcustomizetool/view/activity/DeviceOwnerInstaller.java
@@ -0,0 +1,16 @@
+package com.saradabar.cpadcustomizetool.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saradabar.cpadcustomizetool.R;
+
+public class DeviceOwnerInstaller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_installer);
+    }
+}
